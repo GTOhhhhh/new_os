f8520a6ae873e897d49fd5a56208450c383f6f85
Karl Spiegel <spiegel.karl@gmail.com>
2018-02-25 02:22:57 +1100

    created script, added most programs

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# a simple script for automating my personal set up on a new linux box
+
+# update
+apt update
+apt full-upgrade -y
+
+# add repos
+add-apt-repository ppa:agornostal/ulauncher -y
+add-apt-repository ppa:atareao/telegram -y
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+
+# update repos and install new applications
+apt update
+apt install ulauncher -y
+apt install firefox -y
+apt install telegram -y
+apt install atom -y
