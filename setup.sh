5ed4f816cb11371d4905afee54f33160632de7d5
Karl Spiegel <spiegel.karl@gmail.com>
2018-08-22 00:25:04 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,18 +8,16 @@ apt full-upgrade -y
 
 # add repos
 add-apt-repository ppa:agornostal/ulauncher -y
-add-apt-repository ppa:atareao/telegram -y
 add-apt-repository ppa:nixnote/nixnote2-daily -y
 curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 
 # update repos and install new applications
 apt update
-apt-get update
 # apt install git
 apt install ulauncher -y
 apt install firefox -y
-apt-get install telegram -y
+apt-get install telegram-desktop -y
 apt install atom -y
 apt install nixnote2 -y
 apt install xsel -y
