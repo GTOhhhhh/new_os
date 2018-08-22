b4cebca49db1f9d5dd6f6dd2b1d2461544e85c54
Karl <spiegel.karl@gmail.com>
2018-08-23 03:00:09 +1000

    defaults

--- setup.sh
+++ setup.sh
@@ -9,19 +9,16 @@ apt full-upgrade -y
 
 # add repos
 add-apt-repository ppa:agornostal/ulauncher -y
-add-apt-repository ppa:nixnote/nixnote2-daily -y
-curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-
-# update repos and install new applications
+# add-apt-repository ppa:nixnote/nixnote2-daily -y
+update repos and install new applications
 apt update
 # apt install git
-apt install openjdk-11-jdk
+apt install openjdk-11-jdk -y
 apt install ulauncher -y
-apt install firefox -y
+# apt install firefox -y
 apt-get install telegram-desktop -y
 apt install atom -y
-apt install nixnote2 -y
+# apt install nixnote2 -y
 apt install xsel -y
 apt install redshift -y
 apt install python-nautilus -y
@@ -40,6 +37,3 @@ rm -r /home/$USER/Downloads/*.tar.gz
 
 
 # restart computer
-
-
-
